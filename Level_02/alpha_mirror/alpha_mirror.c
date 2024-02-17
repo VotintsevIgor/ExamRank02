c6d2a513561a58a4b34c326f0a9e608848b0ffee
Igor Votintsev <i.1223.s36@gmail.com>
2024-02-17 13:08:25 +0100

    Did some job on phone

---
+++ Level_02/alpha_mirror/alpha_mirror.c
@@ -0,0 +1,15 @@
+#include <unistd.h>
+
+int	main(int ac, char **av)
+{
+	int	i;
+
+	if (ac == 2)
+	{
+		i = 0;
+		while (*av[1])
+		{
+		a	if ((*av[1] >= 'A' && *av[1] <= 'Z') || (*av[1] >= 'a' && *av[1] <= 'z'))
+
+
+		}
