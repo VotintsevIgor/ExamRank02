9a483de6e2d662d824f4bffaa70be26f0b02c55e
Igor Votintsev <i.1223.s36@gmail.com>
2024-02-17 20:15:13 +0100

    a

--- Level_02/alpha_mirror/alpha_mirror.c
+++ Level_02/alpha_mirror/alpha_mirror.c
@@ -4,12 +4,15 @@ int	main(int ac, char **av)
 {
 	int	i;
 
-	if (ac == 2)
+	i = 0;
+	while (ac == 2 && av[1][i])
 	{
-		i = 0;
-		while (*av[1])
-		{
-		a	if ((*av[1] >= 'A' && *av[1] <= 'Z') || (*av[1] >= 'a' && *av[1] <= 'z'))
-
-
-		}
+		if (av[1][i] >= 'A' && av[1][i] <= 'Z')
+			av[1][i] = 'M' - (av[1][i] - 'N');
+		else if (av[1][i] >= 'a' && av[1][i] <= 'z')
+			av[1][i] = 'm' - (av[1][i] - 'n');
+		write(1, &av[1][i++], 1);
+	}
+	write(1, "\n", 1);
+	return (0);
+}
