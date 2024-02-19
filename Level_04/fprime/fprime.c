833f5915a4534e285717f65b25b8f79c112e8d0f
Igor Votintsev <i.1223.s36@gmail.com>
2024-02-19 14:57:55 +0100

    Did some staff

--- Level_04/fprime/fprime.c
+++ Level_04/fprime/fprime.c
@@ -3,31 +3,29 @@
 
 void	fprime(int num)
 {
-		int	i;
+	int	i;
 
-		i = 2;
-		if (num == 1)
-				printf("1");
-		while (num != 1)
+	i = 2;
+	if (num == 1)
+		printf("1");
+	while (num != 1)
+	{
+		if (num % i == 0)
 		{
-				if (num % i == 0)
-				{
-						printf("%d", i);
-						num /= i;
-						if (num != 1)
-								printf("*");
-				}
-				else
-						i++;
+			printf("%d", i);
+			num /= i;
+			if (num != 1)
+				printf("*");
 		}
+		else
+			i++;
+	}
 }
-
-
 
 int	main(int ac, char **av)
 {
-		if (ac == 2)
-				fprime(atoi(av[1]));
-		printf("\n");
-		return (0);
+	if (ac == 2)
+		fprime(atoi(av[1]));
+	printf("\n");
+	return (0);
 }
