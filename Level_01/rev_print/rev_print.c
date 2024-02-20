a3c72ba49f08e109c50672c347ad898f68d203ae
Igor Votintsev <i.1223.s36@gmail.com>
2024-02-20 17:29:05 +0100

    Tfgjpf

--- Level_01/rev_print/rev_print.c
+++ Level_01/rev_print/rev_print.c
@@ -13,4 +13,5 @@ int	main(int ac, char **av)
 			write(1, &av[1][--i], 1);
 	}
 	write(1, "\n", 1);
+	return (0);
 }
--- Level_01/ulstr/ulstr.c
+++ Level_01/ulstr/ulstr.c
@@ -1,26 +1,17 @@
 #include <unistd.h>
 
-void	ft_change(char *str)
+int	main(int ac, char **av)
 {
 	int	i;
-	
-	i = 0;
-	while(str[i])
-	{
-		if (str[i] >= 'A' && str[i] <= 'Z')
-			str[i] += 32;
-		else if (str[i] >= 'a' && str[i] <= 'z')
-			str[i] -= 32;
-		write(1, &str[i], 1);
-		i++;
-	}
-}
 
-int	main(int argc, char **argv)
-{
-	if (argc == 2)
+	i = 0;
+	while (ac == 2 && av[1][i])
 	{
-		ft_change(argv[1]);
+		if (av[1][i] >= 'A' && av[1][i] <= 'Z')
+			av[1][i] += 32;
+		else if (av[1][i] >= 'a' && av[1][i] <= 'z')
+			av[1][i] -= 32;
+		write(1, &av[1][i++], 1);
 	}
 	write(1, "\n", 1);
 	return (0);
