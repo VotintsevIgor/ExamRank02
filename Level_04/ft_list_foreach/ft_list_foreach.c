df9c5225f0a40c68d1be4e39ce1e9607f829d67e
Igor Votintsev <i.1223.s36@gmail.com>
2024-02-22 23:53:23 +0100

    saasd

--- Level_04/ft_list_foreach/ft_list_foreach.c
+++ Level_04/ft_list_foreach/ft_list_foreach.c
@@ -11,3 +11,38 @@ void	ft_list_foreach(t_list *begin_list, void (*f)(void *))
 		list_ptr = list_ptr->next;
 	}
 }
+
+void multiply_by_2(void *num)
+{
+	*(int *)num *= 2;
+	return ;
+}
+
+#include <stdlib.h>
+
+int main()
+{
+	t_list *list;
+	t_list *head;
+	int arr[] = {1, 2, 3, 4, 5, 6, 7, 8, 9};
+	int	i;
+
+
+	list = malloc(sizeof(t_list));
+	head = list;
+	i = 0;
+	while (i < sizeof(arr) / sizeof(arr[0]))
+	{
+		list->data = &arr[i];
+		if (i < sizeof(arr) / sizeof(arr[0]) - 1)
+		{
+			list->next = malloc(sizeof(t_list));
+			list = list->next;
+		}
+		else
+			list->next = NULL;
+		i++;
+	}
+
+
+}
